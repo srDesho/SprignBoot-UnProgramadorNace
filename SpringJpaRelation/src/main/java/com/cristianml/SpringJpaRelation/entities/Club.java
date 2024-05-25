ed2519473e3bbe2125aaf984eb3a8fbcd9235dbb
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-05-25 19:40:50 -0400

    Relacion @OneToOne

--- SpringJpaRelation/src/main/java/com/cristianml/SpringJpaRelation/entities/Club.java
+++ SpringJpaRelation/src/main/java/com/cristianml/SpringJpaRelation/entities/Club.java
@@ -1,9 +1,6 @@
 package com.cristianml.SpringJpaRelation.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,5 +19,18 @@ public class Club {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    // Hacemos la relación con OneToOne con las siguientes configuraciones:
+    // targetEntity = Coach.class: le decimos con qué entidad la vamos a relacionar en este caso con la clase Coach.
+    // cascade = CacadeType.PERSIST: es para cuando persistimos un registro (guardar) esto automáticamente nos hará
+    // un comportamiento en cascada y nos implementará el insert tanto en la entidad Club como en Coach.
+    // cascade = CacadeType.REMOVE: Si elimino el equipo inmediatamente me elimna el coach relacionado.
+    // cascade = CacadeType.MERGE: Si actualizo un registro en tabla Club tambieén actualizará en la tabla coach.
+
+    // Tip: Es recomendable aprender bien las funcionalidades de estos tipos de cascada, porque si no lo usamos bien
+    // nos puede ocasionar error a la hora de manipular datos.
+    @OneToOne(targetEntity = Coach.class, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "id_coach") // Esta anotación nos sirve para especificar un nombre que queremos.
+    private Coach coach;
 
 }
