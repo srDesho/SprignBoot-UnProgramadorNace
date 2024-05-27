4daf18e6314a3b1f90af7ef036874bc64d3ace9d
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-05-26 20:47:26 -0400

    Relacion @OneToMany

--- SpringJpaRelation/src/main/java/com/cristianml/SpringJpaRelation/entities/Club.java
+++ SpringJpaRelation/src/main/java/com/cristianml/SpringJpaRelation/entities/Club.java
@@ -35,6 +35,22 @@ public class Club {
     @JoinColumn(name = "id_coach") // Esta anotación nos sirve para especificar un nombre que queremos.
     private Coach coach;
 
+    // Relacion OneToMany
+    // El tip para saber qué relación poner es que veamos si la entidad es de uno o muchos, en este caso club es sólo 1
+    // o sea un club puede tener muchos jugadores.
 
+    // Pasamos los sgts parámetros:
+    // targetEntity: Coach.class: le decimos con qué entidad la vamos a relacionar en este caso con la clase Player.
+    // FetchType.LAZY: Si queremos que la carga sea perezosa, o sea que cuando tengamos un objeto Club creado, va a
+    // obtener la plantilla de jugadores sólo cuando la solicite yo, o sea si hacemos uso del método getPlayers, nos va
+    // a obtener los datos desde la DB.
+    // FetchType.EAGLE: A diferencia del LAZY, este ni bien instanciemos un club nos devolverá la lista de players.
+    // Muchas veces no se necesita tener la lista de una tabla relacionada, porque por ejemplo si sólo queremos el nombre
+    // de nuestra entidad Club, si tengo el FetchType me traerá toda la lista, y esto será un guardado en memoria innecesario.
+    // En ese caso hacemos uso de LAZY.
+    // mapperdBy = "club": mappeamos con el nombre del objeto relacionado en la otra clase, esto para que jpa indique que
+    // es ese atributo será la llave foránea en la clase club.
+    @OneToMany(targetEntity = Player.class, fetch = FetchType.LAZY, mappedBy = "club")
+    private List<Player> players;
 
 }
--- SpringJpaRelation/src/main/java/com/cristianml/SpringJpaRelation/entities/Player.java
+++ SpringJpaRelation/src/main/java/com/cristianml/SpringJpaRelation/entities/Player.java
@@ -26,6 +26,14 @@ public class Player {
     private String position;
 
     // Relacion ManyToOne
+    // El tip para saber qué relación poner es que veamos si la entidad es de uno o muchos, en este caso player es de muchos
+    // o sea muchos jugadores sólo pueden pertenecer a un equipo(club).
 
+    // Pasamos los siguientes parámetros:
+    // targetEntity: Club.class: le decimos con qué entidad la vamos a relacionar en este caso con la clase Club.
+    // Si es necesario pasamos un CascadeType, en este caso no lo usaremos.
+    @ManyToOne(targetEntity = Club.class)
+    @JoinColumn(name = "id_club")
+    private Club club;
 
 }
