2e9e5f47bd5cf689b4d8ae5ef98ccb00b1c086a1
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-06-24 16:51:19 -0400

    Inicio de proyecto para JavaTesting con JUnit

---
+++ JavaTestingJunit/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+V
+&src/main/java/com/cristianml/Main.java,e\e\eefa42e602f617a6117fe4a8c76d8988b441d4be
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
---
+++ JavaTestingJunit/src/main/java/com/cristianml/Main.java
@@ -0,0 +1,17 @@
+package com.cristianml;
+
+public class Main {
+
+    // Para hacer pruebas Unitarias con jUnit, debemos agregar la dependencia de JUnit Jupiter Engine
+
+    // En la carpeta "test" de nuestro proyecto es donde se almacena todos los testing, y es aquí donde se debe
+    // tener las mismas carpetas que tiene nuestra carpeta "main" porque es un principio de las unit test.
+    // entonces debemos crear las mismas carpetas y paquetes manualmente, ejemplo la carpetas java y resources,
+    // también creamos los paquetes de la carpeta java.com.cristianml.
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
