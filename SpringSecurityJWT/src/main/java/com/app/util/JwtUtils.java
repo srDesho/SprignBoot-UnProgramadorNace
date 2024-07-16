e57157c2a71eef80113634e02d1c97645447ed8f
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-07-16 18:45:55 -0400

    Creando los metodos extractUsername, getSpecificClaim, getSpecificClaim

--- SpringSecurityJWT/src/main/java/com/app/util/JwtUtils.java
+++ SpringSecurityJWT/src/main/java/com/app/util/JwtUtils.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -12,6 +13,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -88,6 +90,24 @@ public class JwtUtils {
         } catch (JWTVerificationException e) {
             throw new JWTVerificationException("Token invalid, not Authorized.");
         }
+    }
+
+    // Método para extraer el usuario que viene dentro del token
+    public String extractUsername(DecodedJWT decodedJWT) {
+        // Para esto necesitamos tener bien configurado nuestro método createToken en la parte que asignamos el usuario
+        // con el método .withSubject()
+        // debemos retornar un string.
+        return decodedJWT.getSubject().toString();
+    }
+
+    // Método para extraer un claim de nuestro token, los claims son la parte del cuerpo del token, los atributos.
+    public Claim getSpecificClaim(DecodedJWT decodedJWT, String claimName) {
+        return decodedJWT.getClaim(claimName);
+    }
+
+    // Devolviendo todos los claims
+    public Map<String, Claim> getAllClaims(DecodedJWT decodedJWT) {
+        return decodedJWT.getClaims();
     }
 
 }
