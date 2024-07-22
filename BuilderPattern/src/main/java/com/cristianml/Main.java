d5664954b8be837370a0d16233400db4bc4afe5d
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-07-22 07:44:25 -0400

    Inicio de proyecto para Patron Builder

---
+++ BuilderPattern/src/main/java/com/cristianml/Main.java
@@ -0,0 +1,16 @@
+package com.cristianml;
+
+public class Main {
+    public static void main(String[] args) {
+        // Concepto
+        // Builder es un patrón de diseño creacional que nos permite construir objetos complejos paso a paso.
+        // El patrón nos permite producir distintos tipos y representaciones de un objeto empleando el mismo
+        // código de construcción.
+
+        // Este patrón es esencialmente útil cuando tenemos un objeto que se relaciona con diferentes objetos,
+        // nos permite crear cada uno de esos objetos utilizando un solo código de construcción.
+
+        
+
+    }
+}
