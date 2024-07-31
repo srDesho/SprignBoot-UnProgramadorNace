e61f72072e4b92fbd140cd3ef8efa64075b0f5c9
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-07-30 22:07:10 -0400

    Inicio de proyecto para Tipo de Asociaciones en Jpa

---
+++ SpringJpaAssociations/src/main/java/com/cristianml/jpa/associations/SpringJpaAssociationsApplication.java
@@ -0,0 +1,18 @@
+package com.cristianml.jpa.associations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaAssociationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaAssociationsApplication.class, args);
+
+		// Debemos aprender este concepto para saber cómo funciona la asociación.
+
+		// La asociación establece si desde mi entidad actual puedo acceder a la otra entidad relacionada.
+
+	}
+
+}
---
+++ SpringJpaAssociations/src/test/java/com/cristianml/jpa/associations/SpringJpaAssociationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianml.jpa.associations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaAssociationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
