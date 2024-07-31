57a4ca2c4c6f31da87c6c6986ebaf9c90807be87
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-07-30 23:04:04 -0400

    Ejemplo de relacion unidireccional con ManyToOne

---
+++ SpringJpaAssociations/src/main/java/com/cristianml/jpa/associations/unidirectional/many_to_one/School.java
@@ -0,0 +1,24 @@
+package com.cristianml.jpa.associations.unidirectional.many_to_one;
+
+// Debemos aprender este concepto para saber cómo funciona la asociación.
+
+// La asociación establece si desde mi entidad actual puedo acceder a la otra entidad relacionada.
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+// Un colegio puede tener muchos estudiantes
+@Entity(name = "uni_school_many_to_one")
+@Table(name = "uni_school_many_to_one")
+public class School {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
