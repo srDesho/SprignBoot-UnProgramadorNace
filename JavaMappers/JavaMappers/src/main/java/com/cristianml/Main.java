4445685ae79fcdbab82f01297f1ff9df105cd5eb
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-08-07 10:46:02 -0400

    Inicio de proyecto para Mappers

---
+++ JavaMappers/JavaMappers/src/main/java/com/cristianml/Main.java
@@ -0,0 +1,13 @@
+package com.cristianml;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Los mappers nos sirven para convertir un objeto(entidad) a otro(puede ser un DTO).
+        // Los 3 mappers más usados en la industria según Un Programador Nace(en YT) son:
+        // 1. MapStruct
+        // 2. ModelMapper
+        // 3. Orika Mapper
+
+    }
+}
