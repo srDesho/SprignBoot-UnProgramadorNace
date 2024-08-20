9ce24ade8261f86fd6fe29351688988ae5cf909e
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-08-20 06:54:46 -0400

    Manera en la que se mapea un SP en JPA

--- SpringStoredProceduresJPA/src/main/java/com/example/entity/Person.java
+++ SpringStoredProceduresJPA/src/main/java/com/example/entity/Person.java
@@ -12,6 +12,16 @@ import lombok.Setter;
 @AllArgsConstructor
 
 @Entity
+// Usamos la siguiente anotación para que podamos hacer uso de nuestro sp (stored procedure)
+// o en otros términos para poder mapear nuestra SP con JPA.
+@NamedStoredProcedureQuery(
+        // de nombre generalmente se pone el nombre de la clase seguido de un punto y el nombre de nuestro sp de la db.
+        name = "Person.verPersonas",
+        // nombre exacto de nuestro sp de la db.
+        procedureName = "verPersonas",
+        // Aquí mapeamos nuestra clase que es esta misma
+        resultClasses = Person.class
+)
 public class Person {
 
     @Id
