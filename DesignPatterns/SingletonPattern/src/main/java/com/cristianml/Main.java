1359f409d066dd01af3102338ae2f1d7124eb714
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-08-23 19:15:04 -0400

    Inicio de proyecto para Patron Singleton

---
+++ DesignPatterns/SingletonPattern/src/main/java/com/cristianml/Main.java
@@ -0,0 +1,11 @@
+package com.cristianml;
+
+// Patrón Singleton
+// Es un patrón de diseño creacional que nos permite asegurarnos que una clase tenga una sola instancia, a la vez que
+// proporciona un punto de acceso global a dicha instancia.
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
