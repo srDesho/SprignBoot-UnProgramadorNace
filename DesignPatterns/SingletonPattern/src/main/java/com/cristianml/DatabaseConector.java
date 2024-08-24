db6826af17151ba4ef1d637fc1546ed3c6daf81c
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-08-24 19:26:30 -0400

    Reglas para la impelementacion del Patron Singleton

---
+++ DesignPatterns/SingletonPattern/src/main/java/com/cristianml/DatabaseConector.java
@@ -0,0 +1,15 @@
+package com.cristianml;
+
+// El patrón singleton tiene 3 reglas fundamentales para se implementado.
+
+/**
+ * Rules:
+ * 1) Debemos tener un constructo vacío / We need to have an empty constructor.
+ * 2) Debemos tener un atributo privado estático / We need to have a private static attribute.
+ * 3) Debemos tener un método estático que devuelva la instancia / We need to have a static method that returns the instance.
+ */
+public class DatabaseConector {
+
+
+
+}
--- DesignPatterns/SingletonPattern/src/main/java/com/cristianml/Main.java
+++ DesignPatterns/SingletonPattern/src/main/java/com/cristianml/Main.java
@@ -4,6 +4,9 @@ package com.cristianml;
 // Es un patrón de diseño creacional que nos permite asegurarnos que una clase tenga una sola instancia, a la vez que
 // proporciona un punto de acceso global a dicha instancia.
 
+// En este proyecto vamos a hacer un ejemplo en el cual tendremos una clase(que será creada con patrón singleton) que
+// se encargará de hacer conexión a una DB y no será necesario estar creando distintas clases que conecten a la DB
+// desde otras clases.
 public class Main {
     public static void main(String[] args) {
 
