c821984ff452e1e08a1c75af492817c59e8269ce
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-08-27 09:14:06 -0400

    Inicio de proyecto para ApiRest Completa con Test Unitarios y Security

---
+++ SprignBootRestComplete/src/main/java/com/cristianml/SprignBootRestComplete/SprignBootRestCompleteApplication.java
@@ -0,0 +1,13 @@
+package com.cristianml.SprignBootRestComplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprignBootRestCompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprignBootRestCompleteApplication.class, args);
+	}
+
+}
---
+++ SprignBootRestComplete/src/test/java/com/cristianml/SprignBootRestComplete/SprignBootRestCompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianml.SprignBootRestComplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprignBootRestCompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
