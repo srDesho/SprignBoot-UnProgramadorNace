b931e5be5c6be19bb0e9872a89e156ace1f8de06
Cristian Montaño Laime <cristianlai775@gmail.com>
2024-08-27 12:20:01 -0400

    Creando los repositorios para Maker y Product

---
+++ SprignBootRestComplete/src/main/java/com/cristianml/rest/repository/MakerRepository.java
@@ -0,0 +1,9 @@
+package com.cristianml.rest.repository;
+
+import com.cristianml.rest.entities.Maker;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MakerRepository extends CrudRepository<Maker, Long> {
+}
---
+++ SprignBootRestComplete/src/main/java/com/cristianml/rest/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.cristianml.rest.repository;
+
+import com.cristianml.rest.entities.Product;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
