b8586dbb22b6e0b68ec9e89ec58cb27ad981bb60
Cristian Montaño Laime <cristianlai775@gmail.com>
2025-01-23 17:31:53 -0400

    Ejercicio 2, Metodo que lanza una excepcion si el argumento es negativo

--- review-1/java-testing-junit-2/src/test/java/com/cristianml/ExampleTest.java
+++ review-1/java-testing-junit-2/src/test/java/com/cristianml/ExampleTest.java
@@ -7,7 +7,7 @@ public class ExampleTest {
 
     // 1. Método para sumar dos números
     @Test
-    public void sumarTest() {
+    public void testSumar() {
         // Given
         Example example = new Example();
         int numberA = 8;
@@ -21,6 +21,42 @@ public class ExampleTest {
         assertEquals(16, result);
         assertInstanceOf(Integer.class, result);
     }
+
+    // 2. Método que lanza una excepción si el argumento es negativo
+    @Test
+    public void testCheckPositivo() {
+        // Given
+        Example example = new Example();
+        int num = 2;
+        
+        // When
+        boolean result = example.checkPositivo(num);
+
+        // Then
+        assertTrue(result);
+    }
+
+    @Test
+    public void testCheckPositivoError() {
+
+        // Given
+        Example example = new Example();
+        int num = -2;
+
+        // Then
+        assertThrows(IllegalArgumentException.class, () -> {
+            // Aquí dentro es donde ponemos nuestro código que valida que sea un error
+            example.checkPositivo(num);
+        });
+    }
+
+    // Given
+
+
+    // When
+
+
+    // Then
 
 
 }
