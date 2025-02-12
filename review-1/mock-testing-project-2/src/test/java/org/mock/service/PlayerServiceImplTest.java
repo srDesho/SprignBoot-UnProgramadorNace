967a7120ec7ee39d186e841a9547e483309e9d52
Cristian Montaño Laime <cristianlai775@gmail.com>
2025-02-12 17:29:01 -0400

    Primer forma de inyectar los Mocks

--- review-1/mock-testing-project-2/src/test/java/org/mock/service/PlayerServiceImplTest.java
+++ review-1/mock-testing-project-2/src/test/java/org/mock/service/PlayerServiceImplTest.java
@@ -3,22 +3,37 @@ package org.mock.service;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mock.DataProvider;
 import org.mock.persistence.entity.PlayerEntity;
 import org.mock.persistence.repository.PlayerRepositoryImpl;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.List;
 
 public class PlayerServiceImplTest {
 
+
+
+    // 1. Primer forma de inyectar mocks, es una forma muy poco común
+
+    private PlayerRepositoryImpl playerRepository;
+    private PlayerServiceImpl playerService;
+
+    @BeforeEach
+    void init() {
+        this.playerRepository = mock(PlayerRepositoryImpl.class);
+        this.playerService = new PlayerServiceImpl(this.playerRepository);
+    }
+
+
+
     // Nota para nunca olvidar: MOCKITO siempre trabaja con las dependencias.
     @Test
     public void testFindAll() {
-        // GIVEN
-        PlayerRepositoryImpl playerRepository = mock(PlayerRepositoryImpl.class);
-        PlayerServiceImpl playerService = new PlayerServiceImpl(playerRepository);
-
         // WHEN
         when(playerRepository.findAll()).thenReturn(DataProvider.playerListMock());
         List<PlayerEntity> result = playerService.findAll();
